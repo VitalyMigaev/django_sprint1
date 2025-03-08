c2d5f10cb5bfb6ca672cdbe49079cc197001c4f3
Виталий Мигаев <kitalyans1@gmail.com>
2025-03-08 21:08:18 +0300

    Сделано

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -59,5 +59,8 @@ def post_detail(request, id):
 
 
 def category_posts(request, category_slug):
-    filtered_posts = [post for post in posts if post['category'] == category_slug]
+    filtered_posts = [
+        post for post in posts
+        if post['category'] == category_slug
+    ]
     return render(request, 'blog/category.html', {'posts': filtered_posts})
